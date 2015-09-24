e24a7559d46f6688da794664be511466a034808a
lavatahir <lavatahir@5010-038.ODENG2.CARLETON.CA>
2015-09-24 12:12:22 -0400

    zawdfghjk

---
+++ src/AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class AddressBook {
+	private LinkedList<BuddyInfo> addressbook;
+	
+	public AddressBook(){
+		addressbook = new LinkedList<BuddyInfo>();		
+	}
+	
+	public void addBuddy(BuddyInfo buddy){
+		addressbook.add(buddy);
+		System.out.println("Buddy " + buddy.getName() +" has been added.");
+
+	}
+	public void printbook(){
+		for(BuddyInfo b: addressbook){
+			System.out.println("Name: " + b.getName() + "\nAddress: " + b.getHomeaddress() + "\n");
+		}
+	}
+	public void removeBuddy(BuddyInfo buddy){
+		if(addressbook.contains(buddy)){
+			addressbook.remove(buddy);
+			System.out.println("Buddy " + buddy.getName() +" has been removed.");
+		}
+		else{
+			System.out.println("Buddy " + buddy.getName() +" is not on the address book.");
+		}
+	}
+	public static void main(String[] args){
+		System.out.println("Address Book");
+		BuddyInfo buddy = new BuddyInfo("me", "123 LOLO", "123-1244-32542315");
+		AddressBook book = new AddressBook();
+		book.addBuddy(buddy);
+		book.printbook();
+		book.removeBuddy(buddy);
+	}
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,43 @@
+
+public class BuddyInfo {
+	private String name;
+	private String homeaddress;
+	private String phonenumber;
+	
+	
+	public BuddyInfo(String name, String homeaddress, String phonenumber){
+		this.name = name;
+		this.homeaddress = homeaddress;
+		this.phonenumber = phonenumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getHomeaddress() {
+		return homeaddress;
+	}
+	
+	public void setHomeaddress(String homeaddress) {
+		this.homeaddress = homeaddress;
+	}
+	
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public static void main(String[] args){
+		BuddyInfo buddy = new BuddyInfo("tom", "Carleton", "1234");
+		AddressBook ab = new AddressBook();
+		ab.addBuddy(buddy);
+		ab.removeBuddy(buddy);
+	}
+}
